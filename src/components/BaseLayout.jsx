e148e20bef6a91e2d4a21f89fd515b282aaef48c
tyas-susanto <adityas.susanto@arlitapersada.id>
2025-06-15 01:58:33 +0700

    add master data gerbang

---
+++ src/components/BaseLayout.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Button, Layout, Menu, theme } from 'antd';
+import { Link } from 'react-router-dom';
+const { Header, Sider, Content } = Layout;
+const BaseLayout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              label: <Link to={'/'}>Dashboard</Link>,
+            },
+            {
+              key: '3',
+              label: <Link to={'/master-gate'}>Master Data Gerbang</Link>,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? `a` : `b`}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default BaseLayout;
